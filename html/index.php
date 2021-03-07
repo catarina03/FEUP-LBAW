f3e1231fb16242c9b5addd5c5b8474a732a62d6c
Mariana Almeida Truta <up201806543@fe.up.pt>
2021-03-07 10:42:48 +0000

    hoempage is not responsive

--- html/index.php
+++ html/index.php
@@ -3,30 +3,31 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Alt Art</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AltArt</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/45528450c3.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"
         defer>
     </script>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style/custom_navbar.css">
+    <link rel="stylesheet" href="style/homepage.css">
 </head>
 
 <body>
 
     <?php
     include_once('./navbar.php');
+    include_once('./homepage.php');
     draw_nav_bar();
-    echo '<p>Content from PHP.</p>';
-
+    draw_homepage();
   ?>
-    <p>Content from HTML.</p>
+
 
     <?php
     //include_once('./footer.php');
