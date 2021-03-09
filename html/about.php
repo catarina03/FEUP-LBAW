4b95ff9e2b128a0ff0d6f6ce790ef79eb8f99024
Mariana Almeida Truta <up201806543@fe.up.pt>
2021-03-09 00:57:49 +0000

    fixed about us

--- html/about.php
+++ html/about.php
@@ -2,15 +2,15 @@
     function draw_about(){
 ?>
 
-<div class="container about-us-card">
-        <div class="row text-left about-rounded justify-content-center" style="background-color:#8ab5b1;border:none;">
-            <div class="col-lg-8 col-md-12 col-sm-8">
-                <h1 class="text-center about-us-title">About Us</h1>
-                <div class="card"  style="background-color:#8ab5b1;border:none;">
-                    <div class="card-body about-us">
-                        <p">We provide a community website where people can connect and talk to one another 
-                            about their favourite art-related 
-                            topics, keeping everything organized and open to everyone.
+<div class="container about-us-card" style="margin-bottom: 5em;margin-top:4em;">
+    <div class="row text-left about-rounded justify-content-center" style="background-color:#8ab5b1;border:none;">
+        <div class="col-lg-8 col-12">
+            <h1 class="text-center about-us-title">About Us</h1>
+            <div class="card" style="background-color:#8ab5b1;border:none;">
+                <div class="card-body about-us">
+                    <p">We provide a community website where people can connect and talk to one another
+                        about their favourite art-related
+                        topics, keeping everything organized and open to everyone.
                         </p>
                         <ul style="margin-bottom:0;">
                             <p style="margin:0;">Here are some general rules:</p>
@@ -18,49 +18,50 @@
                             <li>Do not post any fake news</li>
                             <li>Do post any abusive content</li>
                             <li>Do not post any offensive content</li>
-                            <p> Any violation to the above rules can be reported,analyzed and deleted by the platform's moderators.</p>
+                            <p> Any violation to the above rules can be reported,analyzed and deleted by the platform's
+                                moderators.</p>
                         </ul>
-                    </div>
                 </div>
-                <h2 class="jusitfy-content-center text-center">Our Team</h2>
-                <div class="row py-5 justify-content-center">
-                    <div class="col-sm-auto">
-                        <figure>
-                            <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg" 
-                            width="100" height="100">
-                            <figcaption class="text-center">Allan</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-sm-auto">
-                        <figure>
-                            <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg" 
-                            width="100" height="100">
-                            <figcaption class="text-center">Catarina</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-sm-auto">
-                        <figure>
-                            <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg" 
-                            width="100" height="100">
-                            <figcaption class="text-center">Mariana</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-sm-auto">
-                        <figure>
-                            <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg" 
-                            width="100" height="100">
-                            <figcaption class="text-center">Rita</figcaption>
-                        </figure>
-                    </div>
             </div>
+            <h2 class="jusitfy-content-center text-center">Our Team</h2>
+            <div class="row py-5 justify-content-center">
+                <div class="col-sm-auto">
+                    <figure>
+                        <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+                            width="100" height="100">
+                        <figcaption class="text-center">Allan</figcaption>
+                    </figure>
+                </div>
+                <div class="col-sm-auto">
+                    <figure>
+                        <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+                            width="100" height="100">
+                        <figcaption class="text-center">Catarina</figcaption>
+                    </figure>
+                </div>
+                <div class="col-sm-auto">
+                    <figure>
+                        <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+                            width="100" height="100">
+                        <figcaption class="text-center">Mariana</figcaption>
+                    </figure>
+                </div>
+                <div class="col-sm-auto">
+                    <figure>
+                        <img src="https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+                            width="100" height="100">
+                        <figcaption class="text-center">Rita</figcaption>
+                    </figure>
+                </div>
             </div>
         </div>
-            
-        
+    </div>
 
-  </div>
-  <!-- End of .container -->
-  <!-- End of .container -->
+
+
+</div>
+<!-- End of .container -->
+<!-- End of .container -->
 <?php
 }
 ?>
@@ -70,11 +71,11 @@
 <!DOCTYPE html>
 <html lang="en-US">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>AltArt</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AltArt</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/45528450c3.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -82,27 +83,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
     </script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style/style.css">
-    <script src="js/script.js" defer></script>
 
-
-    </head>
+</head>
 
 <body>
     <?php
-    include_once('./navbar.php');
-    draw_navbar('authenticated_user');
-    ?>
-
-    <?php
+        include_once('./navbar.php');
+        draw_navbar('authenticated_user');
         draw_about();
+        include_once('./mobilebar.php');
+        draw_mobilebar();
     ?>
 
 </body>
+
 </html>
