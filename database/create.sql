e80b9728d6d905fbfafa4c1aeabf5a19a215b732
Mariana Truta <marianatruta2000@gmail.com>
2021-04-04 09:35:29 +0100

    updated database

--- database/create.sql
+++ database/create.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS tag CASCADE;
 DROP TABLE IF EXISTS post_tag CASCADE;
 DROP TABLE IF EXISTS photo CASCADE;
 DROP TABLE IF EXISTS "comment" CASCADE;
-DROP TABLE IF EXISTS user_comment CASCADE;
 DROP TABLE IF EXISTS saves CASCADE;
 DROP TABLE IF EXISTS support CASCADE;
 DROP TABLE IF EXISTS faq CASCADE;
@@ -26,13 +25,14 @@ DROP TYPE IF EXISTS post_types;
 DROP TYPE IF EXISTS report_motives;
 DROP TYPE IF EXISTS user_types;
 DROP TYPE IF EXISTS frequency_types;
+DROP TYPE IF EXISTS notification_types;
 
 CREATE TYPE category_types AS ENUM ('music', 'tv show', 'cinema', 'theatre', 'literature');
 CREATE TYPE post_types AS ENUM ('news', 'article','review');
 CREATE TYPE report_motives AS ENUM ('Fake news', 'Innapropriate content', 'Abusive content', 'Hate speech', 'Other');
-CREATE TYPE user_types AS ENUM ('Regular' , 'Moderator', 'System Manager');
+CREATE TYPE user_types AS ENUM ('System Manager', 'Moderator', 'Regular');
 CREATE TYPE frequency_types AS ENUM ('Rarely', 'Often', 'Very Often');
-
+CREATE TYPE notification_types AS ENUM ('publish', 'follow', 'vote', 'comment', 'report');
 
 CREATE TABLE tag(
     id SERIAL PRIMARY KEY,
@@ -86,16 +86,13 @@ CREATE TABLE post_tag(
 CREATE TABLE "comment"(
     id SERIAL PRIMARY KEY,
     content text NOT NULL,
+    user_id integer NOT NULL REFERENCES authenticated_user(id) ON DELETE CASCADE,
     comment_date TIMESTAMP DEFAULT NOW() NOT NULL,
-    post_id integer NOT NULL REFERENCES post(id) ON DELETE CASCADE,
-    comment_id integer  REFERENCES "comment"(id) ON DELETE CASCADE
+    post_id integer REFERENCES post(id) ON DELETE CASCADE,
+    comment_id integer REFERENCES "comment"(id) ON DELETE CASCADE,
+    CHECK ((post_id is not null and comment_id is null) or (comment_id is not null and post_id is null))
 );
 
-CREATE TABLE user_comment(  
-    comment_id integer REFERENCES "comment"(id) ON DELETE CASCADE,
-    user_id integer REFERENCES authenticated_user(id) ON DELETE CASCADE,
-    CONSTRAINT pk_user_comment PRIMARY KEY (comment_id, user_id)
-);
 
 CREATE TABLE saves(
     user_id integer REFERENCES authenticated_user(id) ON DELETE CASCADE,
@@ -155,7 +152,7 @@ CREATE TABLE follow_user(
 CREATE TABLE report(
     id SERIAL PRIMARY KEY,
     reported_date DATE NOT NULL DEFAULT NOW(),
-    closed BOOLEAN NOT NULL DEFAULT false,
+    accepted BOOLEAN,
     closed_date DATE,
     motive report_motives NOT NULL,
     user_reporting integer REFERENCES authenticated_user(id),
@@ -170,43 +167,39 @@ CREATE TABLE report(
 CREATE TABLE notification(
     id SERIAL PRIMARY KEY,
     created_date TIMESTAMP DEFAULT NOW() NOT NULL,
-    user_id integer NOT NULL REFERENCES authenticated_user(id) ON DELETE CASCADE
+    notificated_user integer NOT NULL REFERENCES authenticated_user(id) ON DELETE CASCADE,
+    read BOOLEAN DEFAULT FALSE NOT NULL,
+    type notification_types NOT NULL,
+    post_id integer REFERENCES post(id) ON DELETE CASCADE, --> publish notification
+    follower_id integer REFERENCES authenticated_user(id) ON DELETE CASCADE, --> follow notification
+    comment_id integer REFERENCES "comment"(id) ON DELETE CASCADE, --> comment notification
+    voted_comment integer, --> vote comment notification
+    voted_post integer, --> vote post notification
+    voted_user integer, --> vote notification
+    CONSTRAINT pk_vote_post_not FOREIGN KEY (voted_user, voted_post) REFERENCES vote_post(user_id, post_id) ON DELETE CASCADE, --> vote post notification
+    CONSTRAINT pk_vote_comment_not FOREIGN KEY (voted_user, voted_comment) REFERENCES vote_comment(user_id, comment_id) ON DELETE CASCADE, --> vote comment notification
+    report_id integer REFERENCES report(id) ON DELETE CASCADE, --> report notification
+    CHECK(
+        (type = 'publish' AND post_id is not null AND follower_id is null AND comment_id is null AND voted_comment is null AND voted_post is null AND voted_user is null AND report_id is null) --> publish notification
+        OR
+        (type = 'follow' AND post_id is null AND follower_id is not null AND comment_id is null AND voted_comment is null AND voted_post is null AND voted_user is null AND report_id is null) --> follow notification
+        OR
+        (type = 'vote' AND post_id is null AND follower_id is null AND comment_id is null AND voted_comment is not null AND voted_post is null AND voted_user is not null AND report_id is null) --> vote comment notification
+        OR
+        (type = 'vote' AND post_id is null AND follower_id is null AND comment_id is null AND voted_comment is null AND voted_post is not null AND voted_user is not null AND report_id is null) --> vote post notification
+        OR
+        (type = 'comment' AND post_id is null AND follower_id is null AND comment_id is not null AND voted_comment is null AND voted_post is null AND voted_user is null AND report_id is null) --> comment notification
+        OR
+        (type = 'report' AND post_id is null AND follower_id is null AND comment_id is null AND voted_comment is null AND voted_post is null AND voted_user is null AND report_id is not null) --> report notification
+    )
+
 );
 
 
-CREATE TABLE publish_notification(
-    id SERIAL PRIMARY KEY,
-    notification_id integer NOT NULL REFERENCES notification(id) ON DELETE CASCADE,
-    post_id integer NOT NULL REFERENCES post(id) ON DELETE CASCADE
-);
-
-CREATE TABLE follow_notification(
-    id SERIAL PRIMARY KEY,
-    notification_id integer NOT NULL REFERENCES notification(id) ON DELETE CASCADE,
-    follower_id integer NOT NULL REFERENCES authenticated_user(id) ON DELETE CASCADE
-);
-
-CREATE TABLE vote_notification(
-    id SERIAL PRIMARY KEY,
-    notification_id integer NOT NULL REFERENCES notification(id) ON DELETE CASCADE,
-    comment_id integer,
-	user_id integer NOT NULL,
-	post_id integer,
-    CONSTRAINT pk_vote_post_not FOREIGN KEY (user_id, post_id) REFERENCES vote_post(user_id, post_id) ON DELETE CASCADE,
-	CONSTRAINT pk_vote_comment_not FOREIGN KEY (user_id, comment_id) REFERENCES vote_comment(user_id, comment_id) ON DELETE CASCADE,
-    CHECK((comment_id IS NULL AND post_id IS NOT NULL) OR (comment_id IS NOT NULL AND post_id IS NULL))
-);
-
-CREATE TABLE comment_notification(
-    id SERIAL PRIMARY KEY,
-    notification_id integer NOT NULL REFERENCES notification(id) ON DELETE CASCADE,
-    comment_id integer NOT NULL REFERENCES "comment"(id) ON DELETE CASCADE
-);
-
-CREATE TABLE report_notification(
-    id SERIAL PRIMARY KEY,
-    notification_id integer NOT NULL REFERENCES notification(id) ON DELETE CASCADE,
-    reported integer NOT NULL REFERENCES report(id) ON DELETE CASCADE
-);
-
-
+---------------> INDEXES
+CREATE INDEX author_post ON post USING HASH(user_id);
+CREATE INDEX post_date ON post USING BTREE(created_at);
+CREATE INDEX user_tags ON follow_tag USING HASH(user_id);
+CREATE INDEX user_type_idx ON authenticated_user USING HASH(username);
+CREATE INDEX post_comments ON comment USING HASH(post_id);
+CREATE INDEX search_post ON post USING GIN(to_tsvector('english',content || ' ' || title));
