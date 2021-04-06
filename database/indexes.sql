c9ff0e335338495bf96b26f040c682454452f064
Allan Borges de Sousa <up201800149@fe.up.pt>
2021-04-06 15:27:34 +0000

    Update indexes.sql

--- database/indexes.sql
+++ database/indexes.sql
@@ -3,5 +3,7 @@ CREATE INDEX post_date ON post USING BTREE(created_at);
 CREATE INDEX user_tags ON follow_tag USING HASH(user_id);
 CREATE INDEX user_type_idx ON authenticated_user USING HASH(username);
 CREATE INDEX post_comments ON comment USING HASH(post_id);
-CREATE INDEX search_post ON post USING GIN(to_tsvector('english',title || ' ' || content));
-CREATE INDEX search_username ON authenticated_user USING GIN(to_tsvector('english',username));
+CREATE INDEX search_post ON post USING GIN(
+    (setweight(to_tsvector('english',title),'A') ||  setweight(to_tsvector('english',content),'B')));
+
+
