877441667b803fdcc1846dd8a78aa9e1666f378e
Catarina Fernandes <up201806610@fe.up.pt>
2021-04-26 17:14:25 +0100

    Added slider and pagination partials

--- resources/views/pages/category_page.blade.php
+++ resources/views/pages/category_page.blade.php
@@ -16,15 +16,7 @@
             @include('partials.card')
             @include('partials.card')
         </div>
-        <div class="d-flex justify-content-center">
-            <div class="pagination">
-                <a href="#">&laquo;</a>
-                <a href="#" class="active">1</a>
-                <a href="#">2</a>
-                <a href="#">3</a>
-                <a href="#">&raquo;</a>
-            </div>
-        </div>
+        @include('partials.pagination')
     </div>
     @include('partials.filterbox')
 </div>
