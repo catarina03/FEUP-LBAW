3161705c3991db4a98e87c36050413d341035601
Mariana Truta <marianatruta2000@gmail.com>
2021-05-17 23:47:29 +0100

    create axaj js

---
+++ public/js/ajax.js
@@ -0,0 +1,6 @@
+function encodeForAjax(data) {
+    return Object.keys(data).map(function(k){
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+}
+
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -30,6 +30,7 @@
 </script>
 <link rel="stylesheet" href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css')}}">
 
+<script src="{{ asset('js/ajax.js')}}" defer></script>
 <script src="{{ asset('js/script.js')}}" defer></script>
 <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
