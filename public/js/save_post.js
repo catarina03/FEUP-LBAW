cf808ee1878aa79117e72d6e09d05c34f2f50605
Allan Sousa <allanbp.br@gmail.com>
2021-05-17 16:29:51 +0100

    save posts done

--- public/js/save_post.js
+++ public/js/save_post.js
@@ -1,34 +1,34 @@
 
 let s = document.querySelectorAll(".savePost")[0];
+if(s)
+  s.addEventListener("click",function (e) {
+      e.preventDefault();
+      let listClass = s.querySelector("i").classList;
 
-s.addEventListener("click",function () {
-    let listClass = s.querySelector("i").classList;
-
-    if (listClass.contains("bi-bookmark-plus-fill")) {
-      save_post("post",listClass);
-    } else {
-      save_post("delete",listClass);
-    }
-  });
+      if (listClass.contains("bi-bookmark-check-fill")) {
+        save_post("post",listClass);
+      } else {
+        save_post("delete",listClass);
+      }
+    });
 
 
   function save_post(type,listClass){
     var getUrl = window.location;
     var request = new XMLHttpRequest();
-    console.log(getUrl.protocol + "//" + getUrl.host + "/api/post"+'/' + id.innerText + "/save");
     request.open(type, getUrl.protocol + "//" + getUrl.host + "/api/post"+'/' + id.innerText + "/save", true);
     request.onload = function (){
-        if(request.responseText=="SUCCESS"){
-            if(type=="post"){
-              listClass.remove("bi-bookmark-plus-fill");
-              listClass.add("bi-bookmark-check-fill");
-              console.log("ADDED");
-            }
-            else if(type=="delete"){
-              listClass.remove("bi-bookmark-check-fill");
-              listClass.add("bi-bookmark-plus-fill");
-              console.log("REMOVED");
-            }
+        if(request.responseText!="SUCCESS"){
+          if(type=="delete"){
+            listClass.remove("bi-bookmark-plus-fill");
+            listClass.add("bi-bookmark-check-fill");
+            alert("Error removing post from favorites!");
+          }
+          else if(type=="post"){
+            listClass.remove("bi-bookmark-check-fill");
+            listClass.add("bi-bookmark-plus-fill");
+            alert("Error saving post!");
+          }  
         }    
     };
     request.setRequestHeader('X-CSRF-TOKEN',token.getAttribute("content"));
