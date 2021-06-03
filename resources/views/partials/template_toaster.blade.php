27ab098d395a0cdead4c423e959a23f73495cbe8
RitaPeixoto <rita.peixoto2000@gmail.com>
2021-06-04 00:45:50 +0100

    Fix toaster

--- resources/views/partials/template_toaster.blade.php
+++ resources/views/partials/template_toaster.blade.php
@@ -1,5 +1,5 @@
-<div id="post_page_toaster" class="toast-generic-php position-fixed bottom-0 end-0 p-3" style="z-index: 5">
-    <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+<div  class=" position-fixed bottom-0 end-0 p-3" style="z-index: 5">
+    <div id="liveToast" class=" toast-generic-php toast hide" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
             <strong class="me-auto">Action completed!</strong>
             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close" style="background-color:white;"></button>
